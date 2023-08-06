1f3f071a0b12a19243b8bdaf14044446868573e8
Gid3onJura <homebasekorlash@googlemail.com>
2023-08-06 11:03:50 +0200

    footer on landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -13,14 +13,16 @@ const LandingPage = () => {
           <Button>Register</Button>
         </Link>
       </div>
-      <div className="font-extralight absolute bottom-1">
-        <Link href="https://www.flaticon.com/de/kostenlose-icons/kunstliche-intelligenz">
-          Künstliche intelligenz Icons erstellt von Triangle Squad - Flaticon
-        </Link>
-        <Link href="https://www.flaticon.com/de/kostenlose-icons/nichts-gefunden">
-          Empty Icons erstellt von Freepik - Flaticon
-        </Link>
-        <Link href="https://www.flaticon.com/free-icons/refresh">Refresh icons created by Dave Gandy - Flaticon</Link>
+      <div className="font-extralight absolute bottom-1 flex flex-row justify-start p-2 m-2 bg-slate-300">
+        <div className="flex flex-col">
+          <Link href="https://www.flaticon.com/de/kostenlose-icons/kunstliche-intelligenz">
+            Künstliche intelligenz Icons erstellt von Triangle Squad - Flaticon
+          </Link>
+          <Link href="https://www.flaticon.com/de/kostenlose-icons/nichts-gefunden">
+            Empty Icons erstellt von Freepik - Flaticon
+          </Link>
+          <Link href="https://www.flaticon.com/free-icons/refresh">Refresh icons created by Dave Gandy - Flaticon</Link>
+        </div>
       </div>
     </div>
   )
