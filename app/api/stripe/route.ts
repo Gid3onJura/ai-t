b93c825692f7505ad2a41844cf991c19a6d6f810
Gid3onJura <homebasekorlash@googlemail.com>
2023-08-23 18:47:22 +0200

    bug

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -7,6 +7,9 @@ import { absoluteUrl } from "@/lib/utils"
 
 const settingUrl = absoluteUrl("/settings")
 
+export const dynamic = "force-dynamic"
+export const revalidate = 0
+
 export async function GET() {
   try {
     const { userId } = auth()
